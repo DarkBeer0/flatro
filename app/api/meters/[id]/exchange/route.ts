91668a94015961e17b41b53ac8fb7fe48316a48b
DarkBear0 <baphometmarx@gmail.com>
2026-02-27 18:28:51 +0100

    check deploy

--- app/api/meters/[id]/exchange/route.ts
+++ app/api/meters/[id]/exchange/route.ts
@@ -49,7 +49,6 @@ export async function POST(
     ) {
       return NextResponse.json({ error: error.message }, { status: 400 })
     }
-
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
   }
 }
